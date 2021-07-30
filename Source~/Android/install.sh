86aaa2e37cfd8e776232d1b18f7de523ea03630a
Jean-Denis Boivin <jd@boiv.in>
2021-07-30 00:03:00 -0400

    Refactor a bit + added .jar

--- Source~/Android/install.sh
+++ Source~/Android/install.sh
@@ -4,58 +4,29 @@ cd "$(dirname "$0")"
 
 CWD=`dirname $0`
 
-BUILD_DIR="${CWD}/gradle_build"
-LIBS_DIR="${BUILD_DIR}/libs"
-JAVA_DIR="${BUILD_DIR}/src/main/java"
-BIN_DIR="${CWD}/bin"
+BUILD_DIR="${CWD}/lib"
 
-# options
-MODE="Release"
-SCRIPTING_BACKEND="il2cpp"
-UNITY=$1
+echo "ANDROID SDK ROOT:"
+echo ${ANDROID_SDK_ROOT}
 
-echo "Unity Path:"
-echo $UNITY
-
-UNITY_JAVA_LIB="${UNITY}/PlaybackEngines/AndroidPlayer/Variations/${SCRIPTING_BACKEND}/${MODE}/Classes/classes.jar"
-
-# Fix SDK issue
-export ANDROID_SDK_ROOT="${UNITY}/PlaybackEngines/AndroidPlayer/SDK"
-
-#rm -rf "${CWD}/sdk"
-#cp -r "${UNITY}/PlaybackEngines/AndroidPlayer/SDK" "${CWD}/sdk"
-#chmod -R 777 "${CWD}/sdk"
-#export ANDROID_SDK_ROOT="${CWD}/sdk"
-
+# Accept license (couldn't find a way to accept them automatically, need to copy the license file)
 # yes | sdkmanager --licenses
 
-# clean
-rm -rf "${JAVA_DIR}"/*
-rm -rf "${LIB_DIR}"
-rm -rf "${BUILD_DIR}/src"
+# Add your license as a ENV vars to your CI (SDK=30, Build Tools=30.0.3)
+mkdir -p "${ANDROID_SDK_ROOT}/licenses"
+echo -e "\n${ANDROID_LICENSE}" >> "${ANDROID_SDK_ROOT}/licenses/android-sdk-license"
 
-# build
-mkdir -p "${LIBS_DIR}"
-mkdir -p "${BIN_DIR}"
-mkdir -p "${JAVA_DIR}"
-
-cp "${UNITY_JAVA_LIB}" "${LIBS_DIR}"
-#chmod -R 777 "${LIBS_DIR}"
-
-cp -r src/jd "${JAVA_DIR}"
-cp AndroidManifest.xml "${BUILD_DIR}/src/main"
-
+# Build
 ./gradlew clean
 ./gradlew assembleRelease
-cp "${BUILD_DIR}/build/outputs/aar/"*.aar "${BIN_DIR}/WebViewPlugins.aar"
 
-# install
+# Copy build to plugins
 DEST_DIR='../../Plugins/Android'
 mkdir -p "${DEST_DIR}"
-cp "${BIN_DIR}/WebViewPlugins.aar" "${DEST_DIR}/WebViewPlugin.aar"
+cp "${BUILD_DIR}/build/outputs/aar/"*.aar "${DEST_DIR}/WebViewPlugin.aar"
 
-# cleanup
-rm -rf "${CWD}/sdk"
-rm -rf "${JAVA_DIR}"/*
-rm -rf "${LIB_DIR}"
-rm -f "${BUILD_DIR}/src/main/AndroidManifest.xml"
+# Cleanup
+rm -rf "${BUILD_DIR}"
+
+# Copy meta
+cp *.aar.meta ${DEST_DIR}
