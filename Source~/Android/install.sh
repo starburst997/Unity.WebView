594f670245ed5c84c315b0a5bddf8d12591fc16d
Jean-Denis Boivin <jd@boiv.in>
2021-07-30 01:53:05 -0400

    Cleanup script

--- Source~/Android/install.sh
+++ Source~/Android/install.sh
@@ -10,7 +10,6 @@ OLD_ANDROID_HOME=${ANDROID_HOME}
 export ANDROID_HOME="./sdk"
 
 echo ${ANDROID_HOME}
-
 mkdir -p ${ANDROID_HOME}
 
 # Accept license (couldn't find a way to accept them automatically, need to copy the license file)
@@ -21,8 +20,8 @@ mkdir -p "${ANDROID_HOME}/licenses"
 echo -e "\n${ANDROID_LICENSE}" >> "${ANDROID_HOME}/licenses/android-sdk-license"
 
 # Build
-ANDROID_HOME="./sdk" ./gradlew clean
-ANDROID_HOME="./sdk" ./gradlew assembleRelease
+./gradlew clean
+./gradlew assembleRelease
 
 # Revert
 export ANDROID_HOME="${OLD_ANDROID_HOME}"
@@ -31,19 +30,10 @@ export ANDROID_HOME="${OLD_ANDROID_HOME}"
 DEST_DIR='../../Plugins/Android'
 mkdir -p "${DEST_DIR}"
 
-ls -la "./lib/build"
-ls -la "./lib/build/outputs"
-ls -la "./lib/build/outputs/aar"
-
-ls -la "./build"
-ls -la "./build/outputs"
-ls -la "./build/outputs/aar"
-
 cp "./lib/build/outputs/aar/"*.aar "${DEST_DIR}/WebViewPlugin.aar"
-cp "./build/outputs/aar/"*.aar "${DEST_DIR}/WebViewPlugin.aar"
 
 # Cleanup
-rm -rf "${BUILD_DIR}/build"
+rm -rf "./lib/build"
 
 # Copy meta
 cp *.aar.meta ${DEST_DIR}
