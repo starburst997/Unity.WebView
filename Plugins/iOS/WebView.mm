a094f09bca344beacfe356724b8c0cb183648a46
Jean-Denis Boivin <jd@boiv.in>
2021-09-08 22:47:53 -0400

    Another test

--- Plugins/iOS/WebView.mm
+++ Plugins/iOS/WebView.mm
@@ -280,7 +280,7 @@ static NSMutableArray *_instances = [[NSMutableArray alloc] init];
             }*/
             
             // Sidebar is white, otherwise it is black
-            if (scrollView.contentSize.width < webView.frame.size.width && scrollView.contentSize.height >= webView.frame.size.height /*&& (int) scrollView.frame.origin.x <= 0*/) {
+            if (scrollView.contentSize.width < webView.frame.size.width && (int) scrollView.frame.origin.x <= 0) {
                 scrollView.indicatorStyle = UIScrollViewIndicatorStyleWhite;
             } else {
                 scrollView.indicatorStyle = UIScrollViewIndicatorStyleBlack;
