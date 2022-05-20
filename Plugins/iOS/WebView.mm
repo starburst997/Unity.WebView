6e4d84d930f6acf498dedbf4812605a507d75567
Jean-Denis Boivin <jd@boiv.in>
2022-05-20 15:33:11 -0400

    Missing interfaces

--- Plugins/iOS/WebView.mm
+++ Plugins/iOS/WebView.mm
@@ -100,6 +100,10 @@ static BOOL _init = NO;
 
 // Force portrait only on child view controller
 @interface UIViewController (WebViewController)
+
+- (void) viewWillLayoutSubviews;
+- (void) viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator;
+
 @end
 
 @implementation UIViewController (WebViewController)
