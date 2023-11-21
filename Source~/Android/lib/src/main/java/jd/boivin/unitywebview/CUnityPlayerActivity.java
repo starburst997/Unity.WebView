e2ae9dcce307b1a6dd0586c0e8b7c0224b78bc93
Jean-Denis Boivin <jd@boiv.in>
2023-11-21 13:34:11 -0500

    Remove FPS fix, didn't work well on some devices

--- Source~/Android/lib/src/main/java/jd/boivin/unitywebview/CUnityPlayerActivity.java
+++ Source~/Android/lib/src/main/java/jd/boivin/unitywebview/CUnityPlayerActivity.java
@@ -18,23 +18,5 @@ public class CUnityPlayerActivity
         mUnityPlayer = new CUnityPlayer(this);
         setContentView(mUnityPlayer);
         mUnityPlayer.requestFocus();
-        
-        // From: https://forum.unity.com/threads/set-screen-refresh-rate-on-android-11.997247/#post-8164832
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            Window w = getWindow();
-            WindowManager.LayoutParams p = w.getAttributes();
-            Display.Mode[] modes = getDisplay().getSupportedModes();
-            //find display mode with max hz
-            int maxMode = 0;
-            float maxHZ = 60f;
-            for(Display.Mode m:modes) {
-                if (maxHZ <= m.getRefreshRate()) {
-                    maxHZ = m.getRefreshRate();
-                    maxMode = m.getModeId();
-                }
-            }
-            p.preferredDisplayModeId = maxMode;
-            w.setAttributes(p);
-        }
     }
 }
